d13c3238f95841069f14f6f79ad6e240bafcf42e
aanujkhurana <aanujkhurana@gmail.com>
2024-04-01 15:32:13 +1000

    UrlShortner-MicroService✅

--- UrlShortner/index.js
+++ UrlShortner/index.js
@@ -8,24 +8,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
 // Initialize an array to store URLs and their corresponding short URLs
 const urlDatabase = [];
-let currentId = 1;
 
 // Function to validate URL format
 function isValidUrl(url) {
@@ -57,7 +44,7 @@ app.post('/api/shorturl', (req, res) => {
     }
 
     // Generate short URL and save the mapping
-    const shortUrl = currentId++;
+    const shortUrl = urlDatabase.length + 1;
     urlDatabase.push({ original_url: url, short_url: shortUrl });
 
     res.json({ original_url: url, short_url: shortUrl });
