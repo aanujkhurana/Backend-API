4076fcf6318300f1d1ac90ffa4d1eb74ddc4c307
aanujkhurana <aanujkhurana@gmail.com>
2024-04-01 15:19:46 +1000

    UrlShortner-MicroService✅

--- UrlShortner/index.js
+++ UrlShortner/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const dns = require('dns');
@@ -6,11 +7,21 @@ const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-// Middleware for body parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
 
 // Initialize an array to store URLs and their corresponding short URLs
 const urlDatabase = [];
